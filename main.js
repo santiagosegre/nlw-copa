f688f8d1c1a92378db37b85bb6d4a44062938ed7
Santiago Segre <110129047+santiagosegre@users.noreply.github.com>
2022-11-07 19:34:55 +0100

    add news bg

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function createGame(player1, hour, player2) {
   return `
   <li>
-    <img src="./assets/icon-${player1}.svg" alt="flag ${player1}"> 
+    <img src="./assets/teams/icon-${player1}.svg" alt="flag ${player1}"> 
     <strong>${hour}</strong>
-    <img src="./assets/icon-${player2}.svg" alt="flag ${player2}">
+    <img src="./assets/teams/icon-${player2}.svg" alt="flag ${player2}">
   </li>  
   `
 }
 
 let delay = -0.5;
 function createCard(date, day, games) {
-  // delay = delay + 0.5;
+  delay = delay + 0.5;
   return `
       <div class="card" style="animation-delay: ${delay}s">
         <h2>${date} <span>${day}</span></h2>
