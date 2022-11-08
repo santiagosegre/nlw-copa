78971e4ab46a107fcd671ac3429622e6c2e050d4
Santiago Segre <110129047+santiagosegre@users.noreply.github.com>
2022-11-08 02:19:47 +0100

    add change collors

--- main.js
+++ main.js
@@ -1,7 +1,50 @@
+const btnRed = document.querySelector('#btn-red');
+const btnBlue = document.querySelector('#btn-blue');
+const btnGreen = document.querySelector('#btn-green');
+const btnYellow = document.querySelector('#btn-yellow');
+const body = document.querySelector('#body')
+
+btnRed.addEventListener('click', redColor)
+btnBlue.addEventListener('click', blueColor)
+btnGreen.addEventListener('click', greenColor)
+btnYellow.addEventListener('click', yellowColor)
+
+function redColor() {
+  // console.log('Vermelho')
+  body.classList.add('red');
+  body.classList.remove('blue');
+  body.classList.remove('green');
+  body.classList.remove('yellow')
+}
+
+function blueColor() {
+  // console.log('Azul')
+  body.classList.add('blue');
+  body.classList.remove('red');
+  body.classList.remove('green');
+  body.classList.remove('yellow')
+}
+
+function greenColor() {
+  // console.log('Verde')
+  body.classList.add('green');
+  body.classList.remove('blue');
+  body.classList.remove('red');
+  body.classList.remove('yellow')
+}
+
+function yellowColor() {
+  // console.log('Amarelo')
+  body.classList.add('yellow');
+  body.classList.remove('blue');
+  body.classList.remove('green');
+  body.classList.remove('red')
+}
+
 let delay = -0.5;
 
 function createGroup(group, player1, player2, player3, player4) {
-  delay = delay + 0.9;
+  delay = delay + 0.5;
   return `
   <div class="group" style="animation-delay: ${delay}s">
       <h2>${group}</h2>
