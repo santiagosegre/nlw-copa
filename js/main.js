2d849f08a4be252beb84c686c93b844a20d96ee4
Santiago Segre <110129047+santiagosegre@users.noreply.github.com>
2022-11-09 01:34:44 +0100

    update and corrections in backgrounds

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-const btnRed = document.querySelector('#btn-red');
-const btnBlue = document.querySelector('#btn-blue');
-const btnGreen = document.querySelector('#btn-green');
-const btnYellow = document.querySelector('#btn-yellow');
-const body = document.querySelector('#body')
+// const btnRed = document.querySelector('#btn-red');
+// const btnBlue = document.querySelector('#btn-blue');
+// const btnGreen = document.querySelector('#btn-green');
+// const btnYellow = document.querySelector('#btn-yellow');
+// const body = document.querySelector('#body')
 
-btnRed.addEventListener('click', redColor)
-btnBlue.addEventListener('click', blueColor)
-btnGreen.addEventListener('click', greenColor)
-btnYellow.addEventListener('click', yellowColor)
+// btnRed.addEventListener('click', redColor)
+// btnBlue.addEventListener('click', blueColor)
+// btnGreen.addEventListener('click', greenColor)
+// btnYellow.addEventListener('click', yellowColor)
 
-function redColor() {
-  // console.log('Vermelho')
-  body.classList.add('red');
-  body.classList.remove('blue');
-  body.classList.remove('green');
-  body.classList.remove('yellow')
-}
+// function redColor() {
+//   // console.log('Vermelho')
+//   body.classList.add('red');
+//   body.classList.remove('blue');
+//   body.classList.remove('green');
+//   body.classList.remove('yellow')
+// }
 
-function blueColor() {
-  // console.log('Azul')
-  body.classList.add('blue');
-  body.classList.remove('red');
-  body.classList.remove('green');
-  body.classList.remove('yellow')
-}
+// function blueColor() {
+//   // console.log('Azul')
+//   body.classList.add('blue');
+//   body.classList.remove('red');
+//   body.classList.remove('green');
+//   body.classList.remove('yellow')
+// }
 
-function greenColor() {
-  // console.log('Verde')
-  body.classList.add('green');
-  body.classList.remove('blue');
-  body.classList.remove('red');
-  body.classList.remove('yellow')
-}
+// function greenColor() {
+//   // console.log('Verde')
+//   body.classList.add('green');
+//   body.classList.remove('blue');
+//   body.classList.remove('red');
+//   body.classList.remove('yellow')
+// }
 
-function yellowColor() {
-  // console.log('Amarelo')
-  body.classList.add('yellow');
-  body.classList.remove('blue');
-  body.classList.remove('green');
-  body.classList.remove('red')
-}
+// function yellowColor() {
+//   // console.log('Amarelo')
+//   body.classList.add('yellow');
+//   body.classList.remove('blue');
+//   body.classList.remove('green');
+//   body.classList.remove('red')
+// }
